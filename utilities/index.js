2947f7d0bf79cc707b87114e717c260b91f07d7d
joy935 <tupaiv@gmail.com>
2024-05-10 10:50:31 +0200

    added custom f

--- utilities/index.js
+++ utilities/index.js
@@ -57,6 +57,34 @@ Util.buildClassificationGrid = async function(data){
   return grid
 }
 
+/* **************************************
+* Build the single view HTML
+* ************************************ */
+Util.buildSingleView = async function(vehicle){
+  let single 
+  if(single.length > 0) {
+  single = '<div id="single-display">'
+    single += '<h2 id="title">' + vehicle.inv_make + ' ' + vehicle.inv_model + '</h2>'
+    single += '<img src="' + vehicle.inv_image + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + ' on CSE Motors" />'
+    single += '<div class="single-details">'
+        single += '<p><strong>Make:</strong> ' + vehicle.inv_make + '</p>'
+      single += '<p><strong>Model:</strong> ' + vehicle.inv_model + '</p>'
+      single += '<p><strong>Year:</strong> ' + vehicle.inv_year + '</p>'
+      single += '<p><strong>Price:</strong> $' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>'
+    single += '</div>'
+    single += '<div class="single-description">'
+      single += '<h3>Vehicle Description</h3>'
+      single += '<p>Description: ' + vehicle.inv_description + '</p>'
+      single += '<p>Miles: ' + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + '</p>'
+      single += '<p>Color: ' + vehicle.inv_color + '</p>'
+    single += '</div>'  
+  single += '</div>'
+} else {
+  single += '<p class="notice">Sorry, that vehicle could not be found.</p>'
+}
+return single
+}
+
 /* ****************************************
  * Middleware For Handling Errors
  * Wrap other function in this for 
@@ -64,4 +92,4 @@ Util.buildClassificationGrid = async function(data){
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
-module.exports = Util
+module.exports = Util;
