0b61fcd2e0fa4d8b93701650bedd25979886319a
joy935 <tupaiv@gmail.com>
2024-05-10 09:47:13 +0200

    added route

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,4 +6,7 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
+// Route to build the single vehicle view
+router.get("/vehicle/:singleId", invController.buildBySingleId);
+
 module.exports = router;
