64eeee32b6a5db446b513f732940a5526ac68301
joy935 <tupaiv@gmail.com>
2024-05-13 20:37:37 +0200

    changed order

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require('./routes/inventoryRoute');
-const utilities = require('./utilities');
+const utilities = require("./utilities");
+const errorMiddleware = require('./middleware/errorMiddleware');
+const errController = require("./controllers/errController");
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +31,9 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Error route
+app.get('/broken-link', errController.generateError);
+app.use(errorMiddleware);
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
@@ -44,6 +49,7 @@ app.use(async (err, req, res, next) => {
   if(err.status == 404){ message = err.message
   } else {
     message = 'Oh no! There was a crash. Maybe try a different route?'}
+
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
