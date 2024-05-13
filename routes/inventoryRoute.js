ca1e91ea49cac194fc305561663e0e81558a3ff5
joy935 <tupaiv@gmail.com>
2024-05-13 13:43:45 +0200

    removed err route

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -9,8 +9,4 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 // Route to build the single vehicle view
 router.get("/detail/:singleId", invController.buildBySingleId);
 
-// Route to generate an error in the footer
-// router.get('/errors/:generateError', invController.generateError);
-router.get('/broken-link', invController.generateError);
-
 module.exports = router;
