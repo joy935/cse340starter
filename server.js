76b98f41f59a2de8c481f61cfae5115baabecdcc
joy935 <tupaiv@gmail.com>
2024-05-15 17:24:24 +0200

    added middleware bodyparser

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const errController = require("./controllers/errController");
 const session = require("express-session")
 const pool = require('./database/')
 const accountRoute = require('./routes/accountRoute')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -38,6 +39,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+// Body Parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
