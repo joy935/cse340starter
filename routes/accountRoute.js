7bef45b38bc0d77bb394cd067356ee9009b06a00
joy935 <tupaiv@gmail.com>
2024-05-16 11:40:10 +0200

    updated reg route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build the login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -11,6 +12,11 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Route to handle the register form submission
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post(
+    '/register', 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
