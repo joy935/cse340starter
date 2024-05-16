385bf96f0771bd2bb1deb51ca16ab6a0150969a6
joy935 <tupaiv@gmail.com>
2024-05-16 11:37:45 +0200

    added reg check f

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -46,3 +46,27 @@ return [
     .withMessage("Password does not meet requirements."),
 ]
 }
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkRegData = async (req, res, next) => {
+    const { account_firstname, account_lastname, account_email } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      let nav = await utilities.getNav()
+      res.render("account/register", {
+        errors,
+        title: "Registration",
+        nav,
+        account_firstname,
+        account_lastname,
+        account_email,
+      })
+      return
+    }
+    next()
+  }
+  
+  module.exports = validate
