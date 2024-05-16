5f49c29750d8d575c6ae916d415a0fa984110ca7
joy935 <tupaiv@gmail.com>
2024-05-16 10:56:37 +0200

    declared reg rules

---
+++ utilities/account-validation.js
@@ -0,0 +1,48 @@
+const utilities = require(".")
+  const { body, validationResult } = require("express-validator")
+  const validate = {}
+
+/*  **********************************
+*  Registration Data Validation Rules
+* ********************************* */
+validate.registationRules = () => {
+return [
+    // firstname is required and must be string
+    body("account_firstname")
+    .trim()
+    .escape()
+    .notEmpty()
+    .isLength({ min: 1 })
+    .withMessage("Please provide a first name."), // on error this message is sent.
+
+    // lastname is required and must be string
+    body("account_lastname")
+    .trim() // sanitizing function to remove whitespace
+    .escape() // finds any special characters and transforms them into HTML entities
+    .notEmpty() // checks if the value exists
+    .isLength({ min: 2 })
+    .withMessage("Please provide a last name."), // on error this message is sent.
+
+    // valid email is required and cannot already exist in the DB
+    body("account_email")
+    .trim()
+    .escape()
+    .notEmpty()
+    .isEmail() // checks if the value is a valid email address
+    .normalizeEmail() // refer to validator.js docs
+    .withMessage("A valid email is required."),
+
+    // password is required and must be strong password
+    body("account_password")
+    .trim()
+    .notEmpty()
+    .isStrongPassword({ // checks if the value meet specific requirements
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+    })
+    .withMessage("Password does not meet requirements."),
+]
+}
