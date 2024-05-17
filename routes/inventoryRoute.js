979c176f64e33ac6c20cd380e22b5dfa89b06660
joy935 <tupaiv@gmail.com>
2024-05-17 20:52:23 +0200

    added add class and veh routes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,7 +1,8 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
-const invController = require("../controllers/invController")
+const invController = require("../controllers/invController");
+const { route } = require("./static");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -11,5 +12,11 @@ router.get("/detail/:singleId", invController.buildBySingleId);
 
 // Route to build the vehicule management view
 router.get("/", invController.buildManagementView);
+
+// Route to build the add new classification view
+router.get("/type", invController.buildAddClassificationView);
+
+// Route to build the add new vehicle view
+router.get("/detail", invController.buildAddVehicleView);
 
 module.exports = router;
