5c5f9b810a330078b5273061251040c87a2e26fe
joy935 <tupaiv@gmail.com>
2024-05-18 22:27:12 +0200

    added buildAddVeh and addVeh f

--- controllers/invController.js
+++ controllers/invController.js
@@ -96,11 +96,66 @@ invCont.addClassification = async function (req, res, next) {
 * ************************** */
 invCont.buildAddVehicle = async function (req, res, next) {
   let nav = await utilities.getNav()
+  let classificationList = await utilities.buildClassificationList();
+  let inv_image = req.body.inv_image || "/images/vehicles/no-image.png"
+  let inv_thumbnail = req.body.inv_thumbnail || "/images/vehicles/no-image-tn.png"
   res.render("./inventory/add-inventory", {
     title: "Add New Vehicle",
     nav,
-    // errors: null,
+    classificationList,
+    inv_image,
+    inv_thumbnail,
+    errors: null,
   })
+}
+
+/* ***************************
+ *  Process add new vehicle form submission
+ * ************************** */
+invCont.addVehicle = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const {
+    classification_id, 
+    inv_make, 
+    inv_model, 
+    inv_description, 
+    inv_image,
+    inv_thumbnail, 
+    inv_price, 
+    inv_year, 
+    inv_miles, 
+    inv_color } = req.body
+
+  const resVehicle = await invModel.addVehicle(
+    classification_id, 
+    inv_make,
+    inv_model, 
+    inv_description, 
+    inv_image, 
+    inv_thumbnail, 
+    inv_price,
+    inv_year, 
+    inv_miles, 
+    inv_color)
+
+  if (resVehicle.rowCount > 0) { // adjust if needed
+    req.flash(
+      "notice",
+      `This ${inv_make} ${inv_model} was successfully added.`
+    )
+    res.status(201).render("inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null,
+    })
+  } else {
+    req.flash("notice", "Sorry, there was an error adding this vehicle.")
+    res.status(501).render("inventory/add-inventory", {
+      title: "Add a New Vehicle",
+      nav,
+      errors: null,
+    })
+    }
 }
 
 module.exports = invCont;
