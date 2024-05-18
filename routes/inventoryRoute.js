19007edc26fae0211f43a4085f98526c970fa9fa
joy935 <tupaiv@gmail.com>
2024-05-18 16:45:31 +0200

    added post route

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,8 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController");
 const { route } = require("./static");
+const utilities = require("../utilities");
+const invValidate = require('../utilities/inventory-validation')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -14,9 +16,20 @@ router.get("/detail/:singleId", invController.buildBySingleId);
 router.get("/", invController.buildManagementView);
 
 // Route to build the add new classification view
-router.get("/type", invController.buildAddClassificationView);
+router.get("/type", invController.buildAddClassification);
 
 // Route to build the add new vehicle view
-router.get("/detail", invController.buildAddVehicleView);
+// router.get("/detail", invController.buildaddVehicle);
+
+// Route to handle the add new classification form submission
+router.post(
+    "/type", 
+    invValidate.classificationRules(),
+    invValidate.checkClassificationData,
+    utilities.handleErrors(invController.addClassification)
+);
+// router.post("/type", invController.addClassification);
+
+// Route to handle the add new vehicle form submission
 
 module.exports = router;
