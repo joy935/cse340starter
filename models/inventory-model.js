db348d0d472bb7ff74ce439f196fe6de244c7291
joy935 <tupaiv@gmail.com>
2024-05-18 22:27:33 +0200

    added getClassfs and addVeh f

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -69,4 +69,44 @@ async function checkExistingClassification(classification_name){
   }
 }
 
-module.exports = {getClassifications, getInventoryByClassificationId, getVehiculeById, addClassification, checkExistingClassification };
+/* ***************************
+* Get all classifications
+* ************************** */
+async function getClassifications(){
+    return await pool.query(
+      `SELECT * FROM public.classification 
+      ORDER BY classification_name`)
+  }
+
+/* ***************************
+* Get all inventory items by classification_id
+* ************************** */
+async function addVehicle(classification_id, inv_make, 
+  inv_model, inv_description, 
+  inv_image, inv_thumbnail, 
+  inv_price, inv_year, 
+  inv_miles, inv_color){
+
+  try {
+    const sql = `INSERT INTO inventory (
+      classification_id, inv_make, 
+      inv_model, inv_description, 
+      inv_image, inv_thumbnail, 
+      inv_price, inv_year, 
+      inv_miles, inv_color) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
+      RETURNING *`
+
+    return await pool.query(sql,
+      [classification_id, inv_make,
+        inv_model, inv_description,
+        inv_image, inv_thumbnail,
+        inv_price, inv_year, inv_miles,
+        inv_color])
+
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getVehiculeById, addClassification, checkExistingClassification, getClassifications, addVehicle};
