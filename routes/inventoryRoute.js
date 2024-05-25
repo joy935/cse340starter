db307d29678734647e076776917542fb03f47128
joy935 <tupaiv@gmail.com>
2024-05-25 14:03:16 +0200

    added middleware

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,6 +5,7 @@ const invController = require("../controllers/invController");
 const { route } = require("./static");
 const utilities = require("../utilities");
 const invValidate = require('../utilities/inventory-validation')
+const authorisationMiddleware = require("../middleware/authorisationMiddleware");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -16,20 +17,28 @@ router.get("/detail/:singleId", invController.buildBySingleId);
 router.get("/", invController.buildManagementView);
 
 // Route to build the add new classification view
-router.get("/type", invController.buildAddClassification);
+router.get(
+    "/type", 
+    authorisationMiddleware,
+    invController.buildAddClassification);
 // Route to handle the add new classification form submission
 router.post(
     "/type", 
+    authorisationMiddleware,
     invValidate.classificationRules(),
     invValidate.checkClassificationData,
     utilities.handleErrors(invController.addClassification)
 );
 
 // Route to build the add new vehicle view
-router.get("/detail", invController.buildAddVehicle);
+router.get(
+    "/detail", 
+    authorisationMiddleware,
+    invController.buildAddVehicle);
 // Route to handle the add new vehicle form submission
 router.post(
     "/detail",
+    authorisationMiddleware,
     invValidate.vehicleRules(),
     invValidate.checkVehicleData,
     utilities.handleErrors(invController.addVehicle)
@@ -40,17 +49,27 @@ router.get("/getInventory/:classification_id", utilities.handleErrors(invControl
 
 
 // Route to build the view of the update a vehicle or inventory
-router.get("/edit/:singleId", utilities.handleErrors(invController.buildUpdateVehicle));
+router.get(
+    "/edit/:singleId", 
+    authorisationMiddleware,
+    utilities.handleErrors(invController.buildUpdateVehicle));
 // Route to  handle update a vehicle or inventory
 router.post(
     "/update/", 
+    authorisationMiddleware,
     invValidate.vehicleRules(),
     invValidate.checkUpdateVehicleData,
     utilities.handleErrors(invController.updateInventory))
 
 // Route to build the view of the delete a vehicle or inventory
-router.get("/delete/:singleId", utilities.handleErrors(invController.buildDeleteVehicle));
+router.get(
+    "/delete/:singleId", 
+    authorisationMiddleware,
+    utilities.handleErrors(invController.buildDeleteVehicle));
 // Route to handle the delete a vehicle or inventory
-router.post("/delete/", utilities.handleErrors(invController.deleteInventory));
+router.post(
+    "/delete/", 
+    authorisationMiddleware,
+    utilities.handleErrors(invController.deleteInventory));
 
 module.exports = router;
