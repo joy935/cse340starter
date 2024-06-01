6ac7e392ad7cae32f190fbd75dd76f04cc99af06
joy935 <tupaiv@gmail.com>
2024-06-01 15:27:50 +0200

    gathered routes

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,20 +5,9 @@ const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 const regValidate = require('../utilities/account-validation')
 
+// LOGIN ROUTES
 // Route to build the login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
-
-// Route to build the register view
-router.get("/register", utilities.handleErrors(accountController.buildRegister));
-
-// Route to handle the register form submission
-router.post(
-    "/register", 
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-)
-
 // Route to handle the login form submission
 router.post(
     "/login",
@@ -27,11 +16,22 @@ router.post(
     utilities.handleErrors(accountController.accountLogin)
 )
 
+// REGISTER ROUTES
+// Route to build the register view
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+// Route to handle the register form submission
+router.post(
+    "/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+// ACCOUNT MANAGEMENT ROUTES
 // Route to build the account management view
 router.get(
     "/management", 
     utilities.handleErrors(accountController.buildAccounManagement))
-
 // Route to build the update account view
 router.get(
     "/update/:account_id",
@@ -52,10 +52,16 @@ router.post(
     utilities.handleErrors(accountController.updatePassword)
 )
 
+// LOGOUT ROUTE
 // Route to handle the logout
 router.get("/logout", utilities.handleErrors(accountController.logout))
 
+// WISHLIST ROUTES
 // Route to build the wishlist view
 router.get("/wishlist/:account_id", utilities.handleErrors(accountController.buildWishlist))
+// Route to hangle the add to wishlist
+router.post("/wishlist", 
+regValidate.checkWishlistData,
+utilities.handleErrors(accountController.addToWishlist))
 
 module.exports = router;
