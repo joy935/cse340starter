7c9e9e786355e26fa98965686d488ab6cf881f9a
joy935 <tupaiv@gmail.com>
2024-06-01 18:27:56 +0200

    added authmiddlw

---
+++ middleware/authorizeWishlist.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+
+// check JWT token and account type
+function authorisationMiddleware(req, res, next) {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    req.flash("notice", "Please log in to access this page.");
+    return res.redirect("/account/login");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+
+    if (req.user.account_type === 'Employee' || req.user.account_type === 'Admin' || req.user.account_type === 'Client') { 
+      
+      next();
+    } else {
+      req.flash("notice", "Access denied. Insufficient permissions.");
+      return res.redirect("/account/login");
+    }
+  } catch (error) {
+    req.flash("notice", "Invalid request. Please log in again.");
+    return res.redirect("/account/login");
+  }
+}
+
+module.exports = authorisationMiddleware;
